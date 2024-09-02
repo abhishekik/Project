57270dcb81e8ba9878e0b0e6f95946745a5d6bcb
abhishekik <112273948+abhishekik@users.noreply.github.com>
2024-09-02 13:18:55 +0530

    Delete ProjectModule directory

--- ProjectModule/projectORS/src/main/java/com/rays/service/PrescriptionServiceInt.java
+++
@@ -1,9 +0,0 @@
-package com.rays.service;
-
-import com.rays.common.BaseDAOInt;
-import com.rays.common.BaseServiceInt;
-import com.rays.dto.PrescriptionDTO;
-
-public interface PrescriptionServiceInt extends BaseServiceInt<PrescriptionDTO> {
-
-}
